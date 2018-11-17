55ad0de0f587153f3df50b3f5c2156d6824fe1b3
Huey <leejinhuey@gmail.com>
2018-11-17 14:10:12 +0000

    randomised bg & text color

--- retro.js
+++ retro.js
@@ -1,2 +1,37 @@
 // runs in active tab
-console.log('test')
+
+const randElement = arr => arr[Math.floor(Math.random() * arr.length)]
+
+const downgradeBackground = () => {
+  const tiledBackgrounds = [
+    'https://i.imgur.com/lu5XT48.gif',
+    'https://www.warnerbros.com/archive/spacejam/movie/img/bg_stars.gif',
+    'https://www.warnerbros.com/archive/spacejam/movie/cmp/behind/img/bg-behind.gif'
+  ]
+
+  const body = document.querySelector('body')
+  const background = randElement(tiledBackgrounds)
+  console.log(background)
+
+  body.setAttribute('style', `${body.style.cssText}background: url('${background}') !important;`)
+}
+
+const randomiseTextColor = () => {
+  const allText = document.querySelectorAll('p,span,li')
+  const possibleColors = [
+    '#00ff40',
+    '#ffff00',
+    '#ff0000',
+    '#ff00ff',
+    '#ffffff'
+  ]
+  allText.forEach(text => {
+    const color = randElement(possibleColors)
+    text.setAttribute('style', `${text.style.cssText}color: ${color} !important;`)
+  })
+}
+
+window.setTimeout(() => {
+  downgradeBackground()
+  randomiseTextColor()
+}, 1000)
