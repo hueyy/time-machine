fdefec093b9cd43690dd805b8dd08bc55bf3599d
Sam Gao <gaoshan_274@hotmail.com>
2018-11-17 14:47:02 +0000

    added cursors

--- retro.js
+++ retro.js
@@ -1,2 +1,8 @@
 // runs in active tab
-console.log('test')
+var cursorStyle = document.createElement("link");
+cursorStyle.setAttribute("rel", "stylesheet");
+cursorStyle.setAttribute("type", "text/css");
+var cursors = ["hollow", "pyramid", "sword", "triforce"];
+var cursorURL = "cursor-" + cursors[Math.round(Math.random() * cursors.length) % cursors.length] + ".css";
+cursorStyle.setAttribute("href", chrome.runtime.getURL(cursorURL));
+document.head.appendChild(cursorStyle);
